2314e3b3b723e3b7f3a73b81cee395768179db73
u7NGAQhsej <aleksandr.ostromogilskii@gmail.com>
2023-11-22 15:31:39 +0400

    refactor

--- src/layout/Footer.jsx
+++ src/layout/Footer.jsx
@@ -1,14 +1,11 @@
 import React from "react";
 
 export default class Footer extends React.Component {
-    constructor(props){
-        super(props)
-    }
     render(){
         return (
             <footer>
-                <a href="https://github.com/Univercee"><img src="/images/github.svg"></img></a>
-                <a href="https://www.linkedin.com/in/aleksandr-ostromogilskii-769315205/"><img src="/images/linkedin.svg"></img></a>
+                <a href="https://github.com/Univercee"><img src="/images/github.svg" alt="github_link"></img></a>
+                <a href="https://www.linkedin.com/in/aleksandr-ostromogilskii-769315205/"><img src="/images/linkedin.svg" alt="linkedin_link"></img></a>
             </footer>
         )
     }
--- src/views/error404.jsx
+++ src/views/error404.jsx
@@ -21,9 +21,9 @@ export default class ErrorView extends React.Component {
                     <a className="img-license" href="https://useanimations.com">useanimations</a>
                     <a className="img-license" href="http://www.freepik.com">Designed by Bamdewanto / Freepik</a>
                     <div style={{display: 'flex'}}>
-                        <a className="img-license" target="_blank" href="https://icons8.com/icon/81984/movie-beginning">Movie Beginning</a>
+                        <a className="img-license" target="_blank" rel="noreferrer" href="https://icons8.com/icon/81984/movie-beginning">Movie Beginning</a>
                         <span>&nbsp;icon by&nbsp;</span>
-                        <a target="_blank" href="https://icons8.com">Icons8</a>
+                        <a target="_blank" rel="noreferrer" href="https://icons8.com">Icons8</a>
                     </div>
                     <a className="img-license" href="https://www.flaticon.com/free-icons/no-camera" title="no camera icons">No camera icons created by Those Icons - Flaticon</a>
                 </div>
